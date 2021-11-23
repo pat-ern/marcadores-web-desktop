f2c25cef8de4995db86a7600dc9e63dcd30040af
noemimerari123 <no.gonzalez@duocuc.cl>
2021-11-23 16:00:41 -0300

    Cambio Inicio Sesion
    Ahora El menu principal es el inicio de sesion

--- MarcadorWeb/src/vistas/InicioSesion.java
+++ MarcadorWeb/src/vistas/InicioSesion.java
@@ -12,12 +12,12 @@ import javax.swing.JOptionPane;
  *
  * @author 56930
  */
-public class menuGeneral extends javax.swing.JFrame {
+public class InicioSesion extends javax.swing.JFrame {
 
     /**
      * Creates new form menuGeneral
      */
-    public menuGeneral() {
+    public InicioSesion() {
         initComponents();
 
         //CENTRAR LAS VENTANAS
@@ -208,20 +208,21 @@ public class menuGeneral extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(menuGeneral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicioSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(menuGeneral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicioSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(menuGeneral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicioSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(menuGeneral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(InicioSesion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new menuGeneral().setVisible(true);
+                new InicioSesion().setVisible(true);
             }
         });
     }
